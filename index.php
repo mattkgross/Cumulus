8758650597dbd0fdb9f519e8eff3bf0e08ec6e5a
Matt Gross <mattkgross@gmail.com>
2015-04-30 18:59:44 -0600

    Added WOW.js + bootstrap to template

--- index.php
+++ index.php
@@ -5,10 +5,42 @@ $auth_api = "https://accounts.spotify.com";
 
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>TuneSpy</title>
 
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Wow.js Linkage -->
+    <link rel="stylesheet" href="css/animate.css">
+    <script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+
+    <link href="css/main.css" rel="stylesheet">
 </head>
 <body>
+<div class="main_header row">
+    <div class="col-lg-3">Logo Here</div>
+    <div class="col-lg-6">Menu (align right)</div>
+    <div class="col-lg-3">Search/Misc</div>
+</div>
 
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
 
