e913a2bbb110e97f3e4658b9708fc24f21ac40e4
Matt Gross <mattkgross@gmail.com>
2015-05-03 20:34:11 -0600

    Gg Spotify. Going soundcloud.

--- collection/SpotifyAPI/src/Driver.java
+++
@@ -1,6 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        User user = new User();
-        System.out.println("User is authenticated: " + user.isAuthenticated());
-    }
-}
