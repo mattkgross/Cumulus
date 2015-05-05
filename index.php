40b3178db029116789d8e546c353353e04269bc1
Matt Gross <mattkgross@gmail.com>
2015-05-05 15:56:50 -0600

    Fixed script issue

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <!-- Meta Tags -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="keywords" content="TuneSpy, Soundcloud, Music Tracking, Social Music, Music Trends"/>
+    <meta name="keywords" content="Cumulus, Soundcloud, Music Tracking, Social Music, Music Trends"/>
     <meta name="description"
-          content="TuneSpy tracks your Soundcloud listening habits and allows you to view your trends with a beautiful UI."/>
+          content="Cumulus tracks your Soundcloud listening habits and allows you to view your trends with a beautiful UI."/>
     <meta name="robots" content="index,follow"/>
     <meta name="classification" content="Music Tracker, Music Trend Display"/>
     <meta name="audience" content="Music Listeners"/>
@@ -22,10 +22,53 @@
     <meta name="distribution" content="global"/>
     <meta name="copyright" content="Matthew Gross &amp; Derek Baumgartner"/>
 
-    <title>TuneSpy</title>
+    <title>Cumulus</title>
 
-    <!-- Include necessary headers -->
-    <?php require_once('headers/main_header.php'); ?>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- CSS styling for page -->
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-1.11.2.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Wow.js Linkage -->
+    <link rel="stylesheet" href="css/animate.css">
+    <script src="js/wow.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            var wow = new WOW(
+                {
+                    boxClass: 'wow',      // default
+                    animateClass: 'animated', // default
+                    offset: 0,          // default
+                    mobile: true,       // default
+                    live: true        // default
+                }
+            );
+            wow.init();
+
+            // Begin FadeIn
+            $('#overlay').css('opacity', '0.2');
+        });
+    </script>
+
+    <!-- Angular.js -->
+    <script src="js/angular.min.js"></script>
+
+    <!-- Font Imports -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <!--<div class="main_header row">
