13bcc54c98cf161976329dce9dd42cce99c6f56a
Matt Gross <mattkgross@gmail.com>
2015-05-05 20:07:16 -0600

    added nav element for device scaling

--- index.php
+++ index.php
@@ -62,73 +62,18 @@
     <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<!--<div class="main_header row">
-    <div class="col-lg-3"><img src="img/logo_64.png" class="logo_header"></div>
-    <div class="col-lg-6">Menu (align right)</div>
-    <div class="col-lg-3">Search/Misc</div>
-</div>-->
-<!--<nav class="navbar navbar-fixed-top main_header">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown
-                        <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <form class="navbar-form navbar-left" role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown
-                        <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
-                </li>
+<nav class="navbar navbar-fixed-top">
+    <div class="container-fluid">
+        <div id="main_header" class="collapse navbar-collapse wow slideInDown animated" data-wow-delay="4.0s">
+            <ul>
+                <li style="padding-left: 0px;"><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Dashboard</a></li>
+                <li style="padding-right: 0px;"><a href="#">Logout</a></li>
             </ul>
         </div>
     </div>
-</nav>-->
-<div id="main_header" class="wow slideInDown animated" data-wow-delay="4.0s">
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Dashboard</a></li>
-        <li><a href="#">Logout</a></li>
-    </ul>
-</div>
+</nav>
 
 <div id="title_pane">
     <div id="overlay"></div>
